7817ccd2af3ec302b3d46c502cc898a46f99e1e8
mrice <mark.rice.jr@gmail.com>
2019-04-14 23:19:40 -0700

    added timedelta since last release, or search release, also added human readable option

--- sem2cal/package_info.py
+++ sem2cal/package_info.py
@@ -1,7 +1,6 @@
 import requests
 import attr
-# import time
-
+import arrow
 
 @attr.s
 class PackageInfo(object):
@@ -11,6 +10,12 @@ class PackageInfo(object):
     pypi_json = attr.ib(type=dict, init=False)
     release_version = attr.ib(type=str, init=False)
     release_date = attr.ib(type=str, init=False)
+    release_name = attr.ib(type=str, init=False)
+    package_author = attr.ib(type=str, init=False)
+    package_author_email = attr.ib(type=str, init=False)
+    package_maintainer = attr.ib(type=str, init=False)
+    package_maintainer_email = attr.ib(type=str, init=False)
+    license = attr.ib(type=str, init=False)
 
     def _get_release_version(self):
         return self.pypi_json['info']['version']
@@ -21,6 +26,24 @@ class PackageInfo(object):
     def _get_version_release_date(self) -> str:
         return self.pypi_json['releases'][self.release_version][0]['upload_time']
 
+    def _get_author(self) -> str:
+        return self.pypi_json['info']['author']
+
+    def _get_author_email(self) -> str:
+        return self.pypi_json['info']['author_email']
+
+    def _get_maintainer(self) -> str:
+        return self.pypi_json['info']['maintainer']
+
+    def _get_maintainer_email(self) -> str:
+        return self.pypi_json['info']['maintainer_email']
+
+    def _get_license(self) -> str:
+        return self.pypi_json['info']['license']
+
+    def _get_release_name(self) -> str:
+        return self.pypi_json['info']['name']
+
     def _call_pypi(self) -> requests.Response:
         base_url = f"https://pypi.org/pypi/{self.name}/json"
         response = requests.get(base_url)
@@ -30,13 +53,40 @@ class PackageInfo(object):
         self.pypi_json = self._call_pypi().json()
         self.release_version = self._get_release_version()
         self.release_date = self._get_release_date()
+        self.license = self._get_license()
+        self.release_name = self._get_release_name()
 
         if self.search_version:
             self.search_version_release_date = self._get_version_release_date()
 
-    def time_passed_since_update(self) -> str:
-        # TODO should this be Maya formatted?
+        author = self._get_author()
+        if author:
+            self.package_author = author
+
+        author_email = self._get_author_email()
+        if author_email:
+            self.package_author_email = author_email
+
+        maintainer = self._get_maintainer()
+        if maintainer:
+            self.package_maintainer = maintainer
+
+        maintainer_email = self._get_maintainer_email()
+        if maintainer_email:
+            self.package_maintainer_email = maintainer_email
+
+    def time_passed_since_update(self, humanize: bool = False) -> str:
+        # TODO doesnt handle the search_version
         # if version given, it should be from there.
         # time.time()
-        pass
+        if self.search_version_release_date:
+            start_time = arrow.get(self.search_version_release_date)
+        else:
+            start_time = arrow.get(self.release_date)
+        if humanize:
+            time_passed = start_time.humanize(arrow.now())
+        else:
+            time_passed = str(arrow.utcnow() - start_time)
+
+        return time_passed
 
